ab61ce714dcddbb4808c26a27af0dd73dd74b8c7
Your Name <tfikrie@yahoo.com>
2019-04-19 20:58:45 -0400

    closure completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+ 
+  // const firstName = 'Teddy';// Global scop (can be called from any scop )
+  // const fullName = function(){ 
+  //   const lastName = Zenebe; // Local scop (only called with in the full name function)
+  //   return firstName + ' ' + lastName;
+  //  }
+  
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -8,9 +16,12 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  var count = 0;
+  return count++;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
+// Example usage: 
+//const newCounter = counter();
+//newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
